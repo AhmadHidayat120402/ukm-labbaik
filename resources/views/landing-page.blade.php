6970bb9ddc95e756042f0d483edf8680a1b6c3c1
AhmadHidayat120402 <ahmadhidayat120402@gmail.com>
2023-06-12 20:47:15 +0700

    update code v1

--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -4,53 +4,315 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>UKM -LABBAIK | Politeknik Negeri Jember</title>
-
+    <title>UKM -LABAIK | Politeknik Negeri Jember</title>
+    <link rel="shortcut icon" href="img/logo-labbaik.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="css/style1.css">
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container mt-3">
+        <nav class="navbar navbar-expand-lg fixed-top bg-body-tertiary">
+            <div class="container-fluid">
+                <a class="navbar-brand fw-bold" href="#">
+                    <img src="img/logo-labbaik.png" alt="Bootstrap" width="30"><span class="nama-ukm">UKM -
+                        LABAIK</span>
+
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li class="nav-item dropdown mx-3">
+                            <a class="nav-link dropdown-toggle navItem" href="#" role="button"
+                                data-bs-toggle="dropdown" aria-expanded="false">
+                                Pengurus
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Kominfo</a></li>
+                                <li><a class="dropdown-item" href="#">Syi'ar</a></li>
+                                <li><a class="dropdown-item" href="#">PSDM</a></li>
+                                <li><a class="dropdown-item" href="#">KWU</a></li>
+                                <li><a class="dropdown-item" href="#">Kemuslimahan</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item mx-3">
+                            <a class="nav-link active navItem" aria-current="page" href="#tentang-kami">Tentang Kami</a>
+                        </li>
+                        <li class="nav-item mx-3">
+                            <a class="nav-link active navItem" aria-current="page" href="#blog">Blog</a>
+                        </li>
+                        <li class="nav-item mx-3">
+                            <a class="nav-link active navItem" aria-current="page" href="#hubungi-kami">Hubungi Kami</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+    <div class="content-logo">
+        {{-- <div class="image-logo text-center mt-3 p-5">
+            <img src="img/logo-labbaik.png" alt="logo-labbaik" width="250px">
+            <h2 class="mt-5 nama-logo">Lembaga Aktivis Islam Kampus (LABAIK)</h2>
+        </div> --}}
+    </div>
+    <section class="sec-tentang-kami p-2 mt-5">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">
-                <img src="https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Bootstrap"
-                    width="30" height="24"> UKM - LABBAIK
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                    <li class="nav-item dropdown mx-3">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                            aria-expanded="false">
-                            Pengurus
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Kominfo</a></li>
-                            <li><a class="dropdown-item" href="#">Syiar</a></li>
-                            <li><a class="dropdown-item" href="#">PSDM</a></li>
-                            <li><a class="dropdown-item" href="#">KWU</a></li>
-                            <li><a class="dropdown-item" href="#">Kemuslihan</a></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item mx-3">
-                        <a class="nav-link active" aria-current="page" href="#">Tentang Kami</a>
-                    </li>
-                    <li class="nav-item mx-3">
-                        <a class="nav-link active" aria-current="page" href="#">Blog</a>
-                    </li>
-                    <li class="nav-item mx-3">
-                        <a class="nav-link active" aria-current="page" href="#">Hubungi Kami</a>
-                    </li>
-                </ul>
+            <div class="tentang-kami mt-2 pt-5" id="tentang-kami">
+                <h1 class="text-center mt-3 text-ttgkami">Tentang Kami</h1>
+                <div class="row">
+                    <div class="col-md-6">
+                        <h3 class="text-center mt-5 visi">Visi Ketua Umum Labaik</h3>
+                    </div>
+                    <div class="col-md-6">
+                        <h3 class="text-center mt-5 visi">Visi UKM Labaik</h3>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-6 boxs">
+                        <h4 class="text-center fw-bold mt-5 text-visi">
+                            Menjadikan UKM LABAIK sebagai wadah bagi mahasiswa guna memperdalam ilmu agama dan juga
+                            mencetak
+                            mahasiswa yang berakhlak mulia, jujur, serta bertanggung jawab yang diridhoi oleh Allah
+                            subhanahu
+                            wata'ala </h4>
+                    </div>
+                    <div class="col-md-6 boxs">
+                        <h4 class="text-center fw-bold mt-5 text-visi ">
+                            Menjadikan UKM LABAIK sebagai pusat kegiatan kerohanian islam yang terstruktur dan
+                            kolaboratif untuk mewujudkan POLIJE madani</h4>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-6">
+                        <h3 class="text-center mt-5 misi">Misi Ketua Umum Labaik</h3>
+                    </div>
+                    <div class="col-md-6">
+                        <h3 class="text-center mt-5 misi">Misi UKM Labaik</h3>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-6 boxs">
+                        <h4 class="text-center mt-5 text-misi">1. Menciptakan mahasiswa yang berjiwa qurani</h4>
+                    </div>
+                    <div class="col-md-6 boxs">
+                        <h4 class="text-center text-misi mt-5">1. Menjalankan program kerja untuk menciptakan
+                            pemberdayaan anggota secara optimal
+                        </h4>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-6 boxs">
+                        <h4 class="text-center text-misi mt-5">2. Menciptakan mahasiswa yang senantiasa
+                            berakhlak
+                            Al-Qur'an dan as-Sunnah
+                        </h4>
+                    </div>
+                    <div class="col-md-6 boxs">
+                        <h4 class="text-center text-misi mt-5">2. Menjalin dan memperluas jaringan internal dan
+                            eksternal
+                        </h4>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-6 boxs">
+                        <h4 class="text-center text-misi mt-5">3. Mahasiswa mampu menjunjung tinggi nilai-nilai
+                            ketakwaan dan
+                            bernafaskan Islam
+                        </h4>
+                    </div>
+                    <div class="col-md-6 boxs">
+                        <h4 class="text-center text-misi mt-5">3. Sebagai sarana penguatan karakter dan kepribadian
+                            islami anggota
+                        </h4>
+                    </div>
+                </div>
+                <div class="btnReadMore text-center mt-4">
+                    <a href="#" class="btn btn-dark btn-more btn-lg">Lebih banyak</a>
+                </div>
             </div>
         </div>
-    </nav>
+    </section>
 
+    <section class="sec-blog p-2 mt-5 mb-5">
+        <div class="container-fluid">
+            <div id="blog">
+                <h1 class="text-center mt-3 text-ttgkami pt-5">Blog Terbaru</h1>
+                <div class="row">
+                    <div class="col-md-4 mt-3">
+                        <a href="#" class="card boxs border-0">
+                            <img src="img/gili-ketapang.png" alt="Gili Ketapang" class="images">
+                            <div class="card-body">
+                                <h4 class="card-title fw-bold">Gili ketapang</h4>
+                                <p class="d-flex align-items-center gap-2">
+                                    <i class="bx bxs-map"></i>
+                                    Probolinggo,Jawa Timur
+                                </p>
+                                <hr>
+                                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. At, eos!</p>
+                            </div>
+                        </a>
+
+                    </div>
+                    <div class="col-md-4 mt-3">
+                        <a href="#" class="card boxs border-0">
+                            <img src="img/gili-ketapang.png" alt="Gili Ketapang" class="images">
+                            <div class="card-body">
+                                <h4 class="card-title fw-bold">Gili ketapang</h4>
+                                <p class="d-flex align-items-center gap-2">
+                                    <i class="bx bxs-map"></i>
+                                    Probolinggo,Jawa Timur
+                                </p>
+                                <hr>
+                                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. At, eos!</p>
+                            </div>
+                        </a>
+
+                    </div>
+                    <div class="col-md-4 mt-3">
+                        <a href="#" class="card boxs border-0">
+                            <img src="img/gili-ketapang.png" alt="Gili Ketapang" class="images">
+                            <div class="card-body">
+                                <h4 class="card-title fw-bold">Gili ketapang</h4>
+                                <p class="d-flex align-items-center gap-2">
+                                    <i class="bx bxs-map"></i>
+                                    Probolinggo,Jawa Timur
+                                </p>
+                                <hr>
+                                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. At, eos!</p>
+                            </div>
+                        </a>
+
+                    </div>
+                </div>
+            </div>
+            <div class="btnReadMore text-center mt-4">
+                <a href="#" class="btn btn-dark btn-more btn-lg">Lebih banyak</a>
+
+            </div>
+        </div>
+    </section>
+
+    <section class="sec-hub-kami p-2 mt-5 mb-5">
+        <div class="container-fluid">
+            <div id="ikuti-kami">
+                <h1 class="text-center mt-3 text-ttgkami pt-5">Ikuti Kami</h1>
+                <div class="row">
+                    <div class="col-md-4 mt-3">
+                        <a href="#" class="card boxs border-0">
+                            <img src="img/gili-ketapang.png" alt="Gili Ketapang" class="images">
+                            <div class="card-body">
+                                <h4 class="card-title fw-bold">Gili ketapang</h4>
+                                <p class="d-flex align-items-center gap-2">
+                                    <i class="bx bxs-map"></i>
+                                    Probolinggo,Jawa Timur
+                                </p>
+                                <hr>
+                                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. At, eos!</p>
+                            </div>
+                        </a>
+
+                    </div>
+                    <div class="col-md-4 mt-3">
+                        <a href="#" class="card boxs border-0">
+                            <img src="img/gili-ketapang.png" alt="Gili Ketapang" class="images">
+                            <div class="card-body">
+                                <h4 class="card-title fw-bold">Gili ketapang</h4>
+                                <p class="d-flex align-items-center gap-2">
+                                    <i class="bx bxs-map"></i>
+                                    Probolinggo,Jawa Timur
+                                </p>
+                                <hr>
+                                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. At, eos!</p>
+                            </div>
+                        </a>
+
+                    </div>
+                    <div class="col-md-4 mt-3">
+                        <a href="#" class="card boxs border-0">
+                            <img src="img/gili-ketapang.png" alt="Gili Ketapang" class="images">
+                            <div class="card-body">
+                                <h4 class="card-title fw-bold">Gili ketapang</h4>
+                                <p class="d-flex align-items-center gap-2">
+                                    <i class="bx bxs-map"></i>
+                                    Probolinggo,Jawa Timur
+                                </p>
+                                <hr>
+                                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. At, eos!</p>
+                            </div>
+                        </a>
+
+                    </div>
+                    <div class="col-md-4 mt-3">
+                        <a href="#" class="card boxs border-0">
+                            <img src="img/gili-ketapang.png" alt="Gili Ketapang" class="images">
+                            <div class="card-body">
+                                <h4 class="card-title fw-bold">Gili ketapang</h4>
+                                <p class="d-flex align-items-center gap-2">
+                                    <i class="bx bxs-map"></i>
+                                    Probolinggo,Jawa Timur
+                                </p>
+                                <hr>
+                                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. At, eos!</p>
+                            </div>
+                        </a>
+
+                    </div>
+                    <div class="col-md-4 mt-3">
+                        <a href="#" class="card boxs border-0">
+                            <img src="img/gili-ketapang.png" alt="Gili Ketapang" class="images">
+                            <div class="card-body">
+                                <h4 class="card-title fw-bold">Gili ketapang</h4>
+                                <p class="d-flex align-items-center gap-2">
+                                    <i class="bx bxs-map"></i>
+                                    Probolinggo,Jawa Timur
+                                </p>
+                                <hr>
+                                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. At, eos!</p>
+                            </div>
+                        </a>
+
+                    </div>
+                    <div class="col-md-4 mt-3">
+                        <a href="#" class="card boxs border-0">
+                            <img src="img/gili-ketapang.png" alt="Gili Ketapang" class="images">
+                            <div class="card-body">
+                                <h4 class="card-title fw-bold">Gili ketapang</h4>
+                                <p class="d-flex align-items-center gap-2">
+                                    <i class="bx bxs-map"></i>
+                                    Probolinggo,Jawa Timur
+                                </p>
+                                <hr>
+                                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. At, eos!</p>
+                            </div>
+                        </a>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="sec-hub-kami p-2 mt-5 mb-5">
+        <div class="container-fluid">
+            <div id="ikuti-kami">
+                <div class="row">
+                    <div class="col-md-8">
+                        <h1 class="text-center mt-3 text-ttgkami pt-5">UKM LABAIK</h1>
+                    </div>
+                    <div class="col-md-4">
+                        <h3 class="text-center mt-3 pt-5">Link Penting</h3>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous">
     </script>
